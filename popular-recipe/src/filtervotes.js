a180dec9e93331f63ff3455dd6d8de67f6d1ff2c
Eniko BakosDemeter <enidemi@gmail.com>
2014-11-25 11:01:55 +0100

    Can read file contents.

--- popular-recipe/src/filtervotes.js
+++ popular-recipe/src/filtervotes.js
@@ -1,6 +1,15 @@
 
+
 function filtervotes() {
 
 }
 
+function readFile(textfile) {
+	fs = require('fs')
+	var fileContent = fs.readFileSync(textfile).toString()
+	return fileContent;
+
+}
+
 module.exports.filtervotes = filtervotes;
+module.exports.readFile = readFile;
--- popular-recipe/test/test.js
+++ popular-recipe/test/test.js
@@ -1,7 +1,14 @@
 
 var assert = require("assert")
 var filtervotes = require('../src/filtervotes.js').filtervotes;
+var readFile = require('../src/filtervotes.js').readFile;
 
 suite('filter recipe votes', function() {
+	test('read sample file should output line count 13', function() {
+		assert.equal( "4\ntilak +\ntilak +\ntilak -\ntilak +\n3\nratna +\nshashi -\nratna -\n3\nbhavani -\nbhavani +\nbhavani -", readFile("/Users/enidemi/workspace/coding-katas/popular-recipe/testfile.txt"));
+	});
+	// test('read sample file should output line count 13', function() {
+	// 	assert.equal( 13, readFile("/Users/enidemi/workspace/coding-katas/popular-recipe/testfile.txt"));
+	// });
 
 });
