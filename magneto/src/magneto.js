b4317d8845a0b266ebe04b802452197c65b509e5
Eniko BakosDemeter <enidemi@gmail.com>
2014-11-27 11:08:33 +0100

    distance test OK

--- magneto/src/magneto.js
+++ magneto/src/magneto.js
@@ -1,4 +1,5 @@
 
+
 var magnetoEffect = function magnetoEffect(radius, magneticPoints) { 
 	this.radius = radius;
 	this.magneticPoints = magneticPoints;
@@ -9,3 +10,16 @@ var magnetoEffect = function magnetoEffect(radius, magneticPoints) {
 };
 
 module.exports.magnetoEffect = magnetoEffect;
+
+var Point = function Point(x, y) {
+	this.x = x;
+	this.y = y;
+}
+
+Point.prototype = {
+	distance: function distance(other) {
+		return Math.sqrt(Math.pow((this.x-other.x),2) + Math.pow((this.y-other.y),2));
+	}
+}
+
+module.exports.Point = Point;
--- magneto/test/test.js
+++ magneto/test/test.js
@@ -3,15 +3,28 @@
 var assert = require('assert');
 
 var magnetoEffect = require('../src/magneto.js').magnetoEffect;
+var Point = require('../src/magneto.js').Point;
 
 suite('magnetoEffect', function() {
 
+  test( 'point test', function() {
+  	var point = new Point(3, 4);
+    assert.equal(3, point.x)
+    assert.equal(4, point.y)
+  });
 
+  test( 'distance test', function() {
+  	var point1 = new Point(3, 4);
+  	var point2 = new Point(5, 4);
+    assert.equal(2, point1.distance(point2))
+    assert.equal(2, point2.distance(point1))
+  });
 
-  test( 'test', function() {
-  	var ourMagnetoEffect = new magnetoEffect(3, [50,50]);
+  test( 'magnetoeffect test', function() {
+  	var ourMagnetoEffect = new magnetoEffect(3, [new Point(30, 50),]);
     assert.equal( 3, ourMagnetoEffect.radius)
-    assert.notEqual([50,50], ourMagnetoEffect.magneticPoints)
+    assert.equal(30, ourMagnetoEffect.magneticPoints[0].x)
+	assert.equal(50, ourMagnetoEffect.magneticPoints[0].y)
   });
 
   // test( 'test', function() {
