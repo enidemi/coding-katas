1c5a5f31be6e60f958553c3815378df09f5ca343
Eniko BakosDemeter <enidemi@gmail.com>
2014-11-27 10:52:16 +0100

    constructor

--- magneto/src/magneto.js
+++ magneto/src/magneto.js
@@ -1,9 +1,11 @@
 
-var magnetoEffect = { 
-	method1: function( param ) {
-	},
-	method2: function( param ) {
-	}
+var magnetoEffect = function magnetoEffect(radius, magneticPoints) { 
+	this.radius = radius;
+	this.magneticPoints = magneticPoints;
+	// method1: function( param ) {
+	// },
+	// method2: function( param ) {
+	// }
 };
 
 module.exports.magnetoEffect = magnetoEffect;
--- magneto/test/test.js
+++ magneto/test/test.js
@@ -6,8 +6,19 @@ var magnetoEffect = require('../src/magneto.js').magnetoEffect;
 
 suite('magnetoEffect', function() {
 
+
+
   test( 'test', function() {
-    
+  	var ourMagnetoEffect = new magnetoEffect(3, [50,50]);
+    assert.equal( 3, ourMagnetoEffect.radius)
+    assert.notEqual([50,50], ourMagnetoEffect.magneticPoints)
   });
+
+  // test( 'test', function() {
+  // 	var ourMagnetoEffect = new magnetoEffect(3, [50,50]);
+  // 	console.log(ourMagnetoEffect) 
+  // 	assert.notEqual(undefined, ourMagnetoEffect)
+  //   assert.equal( 3, ourMagnetoEffect.radius)
+  // });
 
 });
