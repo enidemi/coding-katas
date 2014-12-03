92e029ec52038928d2dd7aa10795055ca62840c0
Eniko BakosDemeter <enidemi@gmail.com>
2014-12-03 11:03:58 +0100

    return 1 filename from the directory

--- iterator/test/test.js
+++ iterator/test/test.js
@@ -10,7 +10,7 @@ chai.use(sinonChai);
 
 var iterator = require('../src/iterator.js').iterator;
 
-var testRandom = 1;
+var testRandom = 2;
 var testFiles = ["1.txt","2.txt","3.txt","4.txt","5.txt","6.txt","7.th"];
 
 suite('iterator', function() {
@@ -25,6 +25,13 @@ suite('iterator', function() {
     var testData = sinon.stub();
     testData.withArgs("/var/files").returns(testFiles);
     assert.deepEqual( testFiles, iterator(testData("/var/files")) );
+  });
+    test( 'return a single filename', function() {
+    var testData = sinon.stub();
+    testData.withArgs("/var/files").returns(testFiles);
+    var testRandomData = sinon.stub();
+    testRandomData.withArgs("/var/files").returns(testFiles.slice(0,[testRandom-1]));
+    assert.deepEqual( ["1.txt"], iterator(testRandomData("/var/files")) );
   });
 
 });
